859050a81d91e70af3db39b4e929c07fc63e1536
ltiseanu <ltiseanu@bitdefender.com>
2022-11-25 19:31:08 +0200

    Using Firebase API to SignUp-In a user

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,36 +1,83 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [messageRegister, setMessageRegister] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInput = useRef();
+  const passInput = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitFct = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInput.current.value;
+    const enteredPass = passInput.current.value;
+
+    // maybe some validation
+
+    setIsLoading(true);
+    let urlAPI;
+    let apiMessage;
+    if (isLogin) { // login
+      urlAPI = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAAKXSX-8HViGef8b_tGY75oNNW2pVaLgE";
+    } else { // register
+      urlAPI = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAAKXSX-8HViGef8b_tGY75oNNW2pVaLgE";
+    }
+
+      fetch(urlAPI, {
+          method: 'POST',
+          body: JSON.stringify({ email: enteredEmail, password: enteredPass, returnSecureToken: true }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(resp => {
+          setIsLoading(false);
+          if (resp.ok) {
+            return resp.json().then(data => {
+              console.log(1, data);
+              setMessageRegister('You are now loggedin!');
+            });
+          } else { // errors
+            return resp.json().then(data => {
+              console.log(2, data);
+              if (data && data.error && data.error.message) {
+                apiMessage = data.error.message;
+              }
+              setMessageRegister(apiMessage);
+            });
+          }
+        });
+
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <i className={classes.message}>{messageRegister}</i>
+      <form onSubmit={submitFct}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailInput} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passInput} required />
         </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
+        {!isLoading &&
+          <div className={classes.actions}>
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+            <button type='button' className={classes.toggle} onClick={switchAuthModeHandler}>
+              {isLogin ? 'Create new account' : 'Login with existing account'}
+            </button>
+          </div>
+        }
+        {isLoading && <p className={classes.loading}>Loading...</p>}
       </form>
     </section>
   );
